2eb628416d6733227d003cd464acc30e4fd87e62
Reifhel <rafaelherdt1@gmail.com>
2025-07-10 10:16:06 -0300

    feat: add create question, create room and get room question

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,8 +1,8 @@
-import { reset, seed } from 'drizzle-seed';
-import { db, sql } from './connection.ts';
-import { schema } from './schema/index.ts';
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
 
-await reset(db, schema);
+await reset(db, schema)
 
 await seed(db, schema).refine((f) => {
   return {
@@ -13,10 +13,13 @@ await seed(db, schema).refine((f) => {
         description: f.loremIpsum(),
       },
     },
-  };
-});
+    questions: {
+      count: 20,
+    },
+  }
+})
 
-await sql.end();
+await sql.end()
 
 // biome-ignore lint/suspicious/noConsole: Only used on dev
-console.log('Database seeded');
+console.log('Database seeded')
